7f1a878a1ef9fedd193ace742fadcb12d3445339
Dipesh Gaikar <93652992+Dipeshgaikar23@users.noreply.github.com>
2025-07-11 16:36:04 +0530

    Update axios.js
    added the live backend url

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://url-shortner-backend-znzo.onrender.com",
     withCredentials: true,
     timeout: 10000
 })
